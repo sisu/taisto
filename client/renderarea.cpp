1c11bf8fa38783b7229ddb69ec74855e4d0228cb
Mikko Sysikaski <sisu@taiyaki.localdomain>
2009-10-28 01:36:23 +0200

    Made renderarea take locations as proper locations instead of pixel coordinates.

--- client/renderarea.cpp
+++ client/renderarea.cpp
@@ -18,16 +18,19 @@ void RenderArea::paintEvent(QPaintEvent * /* event */)
     //painter.setPen(Qt::NoPen);
     painter.setBrush(QBrush(QColor(90,240,90)));
     for(int i=0;i<players.size();i++) {
-        double x=width/2 + centerx - players[i].x;
-        double y=height/2 +centery - players[i].y;
+		double px = players[i].x * SQUARE, py = players[i].y * SQUARE;
+        double x=width/2 + centerx - px;
+        double y=height/2 + centery - py;
         if(x+RADIUS>=0&&y+RADIUS>=0&&x-RADIUS<width&&y-RADIUS<height) {
+			qDebug()<<"asd"<<x<<y;
             painter.setBrush(QBrush(QColor(90,240,90)));
-            painter.drawEllipse(x*SQUARE-RADIUS,y*SQUARE-RADIUS,RADIUS*2,RADIUS*2);
+            painter.drawEllipse(x-RADIUS,y-RADIUS,RADIUS*2,RADIUS*2);
             painter.setBrush(QBrush(QColor(190,140,90)));
-            painter.drawEllipse(x*SQUARE+(RADIUS-8)*sin(players[i].direction),y*SQUARE+(RADIUS-8)*cos(players[i].direction),3,3);
+            painter.drawEllipse(x+(RADIUS-8)*sin(players[i].direction),y+(RADIUS-8)*cos(players[i].direction),3,3);
         }
 
     }
+#if 0
     for(int i=0;i<bots.size();i++) {
         double x=width/2 + centerx - players[i].x;
         double y=height/2 +centery - players[i].y;
@@ -38,6 +41,7 @@ void RenderArea::paintEvent(QPaintEvent * /* event */)
             painter.drawEllipse(x*SQUARE+(RADIUS-8)*sin(bots[i].direction),y*SQUARE+(RADIUS-8)*cos(bots[i].direction),3,3);
         }
     }
+#endif
 }
 
 
