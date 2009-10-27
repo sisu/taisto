67b2679a411ef3354731b6b438ba8537de5717d2
Mika Laitinen <mika@chessbase.(none)>
2009-10-28 01:34:54 +0200

    Added a couple of directions (just visual look)

--- client/window.cpp
+++ client/window.cpp
@@ -1,28 +1,34 @@
- #include <QtGui>
- #include "renderarea.h"
- #include "window.h"
- Window::Window()
- {      
-     setFixedSize(600,600);
-     renderArea = new RenderArea;
-     QVBoxLayout *layout = new QVBoxLayout;
-     layout->addWidget(renderArea);
-     setLayout(layout);
-     setWindowTitle("simo");
+#include <QtGui>
+#include "renderarea.h"
+#include "window.h"
+Window::Window()
+{      
+	setFixedSize(600,600);
+	renderArea = new RenderArea;
+	QVBoxLayout *layout = new QVBoxLayout;
+	layout->addWidget(renderArea);
+	setLayout(layout);
+	setWindowTitle("simo");
 
- }
+}
 
- void Window::draw(double x, double y, QList<Object> & players, QList<Object>& bots) {
-     renderArea->draw(x, y, players,bots);
-     //"Piirrä"
- }
+void Window::draw(double x, double y, QList<Object> & players, QList<Object>& bots) {
+	renderArea->draw(x, y, players,bots);
+	//"Piirrä"
+}
 
- void Window::keyPressEvent(QKeyEvent* event) {
-     switch(event->key()) {
-         case Qt::Key_Left:
-            qDebug("moro");
+void Window::keyPressEvent(QKeyEvent* event) {
+	switch(event->key()) {
+		case Qt::Key_Right:
+		    break;
+		case Qt::Key_Left:
+			qDebug("moro");
+			break;
+		case Qt::Key_Up:
+			break;
+        case Qt::Key_Down:
             break;
-         default:
-            break;
-     }
- }
+		default:
+			break;
+	}
+}
