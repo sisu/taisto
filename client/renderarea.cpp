4c798bef4a14e9a6240673ccf5cfc238cc994b07
Jussi Kokkala <yuizy@trippi.Elisa>
2009-10-28 07:45:15 +0200

    Taustaväri

--- client/renderarea.cpp
+++ client/renderarea.cpp
@@ -20,13 +20,15 @@ void RenderArea::paintEvent(QPaintEvent * /* event */)
 {
     width=this->size().width();
     height=this->size().height();
-    //    qDebug()<<width<<height;
     QPainter painter(this);
     painter.setRenderHint(painter.Antialiasing,true);
+    
+	painter.setBrush(QBrush(QColor(220,210,180)));
+    painter.drawRect(0,0,width,height);    
 	painter.translate(0, height);
 	painter.scale(1,-1);
-    //painter.setPen(Qt::NoPen);
-
+    
+    
     //Borders
 	painter.setBrush(QBrush(QColor(40,40,40)));
 
@@ -34,9 +36,6 @@ void RenderArea::paintEvent(QPaintEvent * /* event */)
         double over=width/2-centerx;
         painter.drawRect(0,0,(int)(over),(int)(height));
     }
-   // qDebug()<<centerx+width/2<<engine.area.w*SQUARE;
-    //   qDebug()<<centerx<<centery;
-//    painter.drawEllipse(centerx+width/2,100,10,10);
     if(centerx+width/2>engine.area.w*SQUARE) {
         double over=centerx+width/2-engine.area.w*SQUARE;
         painter.drawRect((int)(width-over),0,(int)(over),(int)(height));
@@ -47,24 +46,6 @@ void RenderArea::paintEvent(QPaintEvent * /* event */)
 
     //Boxes
 
-	/*
-    double sx= centerx/SQUARE;
-    double sy= centery/SQUARE;
-    int topx=max(0,(int)( sx - width/2/SQUARE));
-    int topy=max(0,(int)(sy-height/2/SQUARE));
-    int botx=min(engine.area.w-1,(int)(sx+width/2/SQUARE));
-    int boty=min(engine.area.data.size()/engine.area.w-1,(int)(sy+height/2/SQUARE));
-    qDebug()<<"Piirretään "<<topx<<topy<<botx<<boty;
-    painter.setBrush(QBrush(Qt::NoBrush));
-    for(int xa=topx;xa<=botx;xa++) {
-        for(int ya=topy;ya<=boty;ya++) {
-        if(engine.area.data[ya*engine.area.w+xa]!=0||true) painter.drawRect(
-                (xa-topx)*SQUARE + (sx-(int)(sx))*SQUARE ,
-                (ya-topy)*SQUARE + (sy-(int)(sy))*SQUARE ,
-                SQUARE,SQUARE);
-        }
-    }*/
-//	qDebug()<<centerx<<centery;
 	Area& a = engine.area;
 	double w2=width/2, h2=height/2;
 	int startx = max(0, (centerx-w2)/SQUARE);
@@ -81,30 +62,6 @@ void RenderArea::paintEvent(QPaintEvent * /* event */)
 		}
 	}
 
-    /*
-       qDebug()<<"Topleft "<<max(0,(int)((centerx-width/2)/SQUARE))<<max(0,(int)((centery-height/2)/SQUARE));
-       qDebug()<<"Btmright "<<(int)((centerx+width/2)/SQUARE)<<(int)((centery+height/2)/SQUARE);
-       for(int xa=max(0,(int)((centerx-width/2)/SQUARE));xa<=(int)(1+(centerx+width/2)/SQUARE);xa++) {
-       for(int ya=max((int)((centery-height/2)/SQUARE),0);ya<=(int)(1+(centery+height/2)/SQUARE);ya++) {
-       painter.drawRect(xa*SQUARE,ya*SQUARE,SQUARE,SQUARE);
-       }
-       }*/
-
-
-
-
-#if 0
-    for(int i=0;i<bots.size();i++) {
-        double x=width/2 + centerx - players[i].x;
-        double y=height/2 +centery - players[i].y;
-        if(x+RADIUS>=0&&y+RADIUS>=0&&x-RADIUS<width&&y-RADIUS<height) {
-            painter.setBrush(QBrush(QColor(190,120,90)));
-            painter.drawEllipse(x*SQUARE-RADIUS,y*SQUARE-RADIUS,RADIUS*2,RADIUS*2);
-            painter.setBrush(QBrush(QColor(190,140,90)));
-            painter.drawEllipse(x*SQUARE+(RADIUS-8)*sin(bots[i].direction),y*SQUARE+(RADIUS-8)*cos(bots[i].direction),3,3);
-        }
-    }
-#endif
     //Player
     for(int i=0;i<engine.players.size();i++) {
         double px = engine.players[i].x * SQUARE, py = engine.players[i].y * SQUARE;
