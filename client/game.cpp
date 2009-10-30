4f621808689b55471fa3d987dc0b0bc6333defe5
Mikko Sysikaski <sisu@taiyaki.localdomain>
2009-10-30 02:24:06 +0200

    Added removing old lightning in client.

--- client/game.cpp
+++ client/game.cpp
@@ -56,6 +56,15 @@ void Game::go() {
 		qDebug()<<conn.state();
 		end();
 	}
+
+	
+	// Remove old lightings
+	for(int i=0; i<engine.lightnings.size(); ++i) {
+		if (engine.lightnings[0].first.elapsed()>100) {
+			engine.lightnings[0] = engine.lightnings.back();
+			engine.lightnings.pop_back();
+		}
+	}
 }
 void Game::end() {
 	qDebug()<<"disconnected";
